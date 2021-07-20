403fd8215b56dfa4f37a398024d09faed93c316b
soutivath <www.soutivath2012@gmail.com>
2021-07-20 15:34:39 +0700

    fix notification

--- app/Http/Resources/NotificationResource.php
+++ app/Http/Resources/NotificationResource.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Resources\Json\JsonResource;
-use File;
 use Config;
+use File;
+use Illuminate\Http\Resources\Json\JsonResource;
+
 class NotificationResource extends JsonResource
 {
     /**
@@ -16,20 +17,21 @@ class NotificationResource extends JsonResource
     public function toArray($request)
     {
         $anImage = array();
-        $files= File::files(public_path('/buddhist_images/'.$this->buddhist->image_path."/"));
-        
-        $file_path = pathinfo($files[0]);
-        \array_push($anImage,Config("values.APP_URL").":".$_SERVER["SERVER_PORT"].
-        "/"."buddhist_images/".$this->buddhist->image_path."/".$file_path['basename']);
+        $files = File::files(public_path('/buddhist_images/' . $this->buddhist->image_path . "/"));
 
-        
+        $file_path = pathinfo($files[0]);
+        \array_push($anImage, Config("values.APP_URL") . ":" . $_SERVER["SERVER_PORT"] .
+            "/" . "buddhist_images/" . $this->buddhist->image_path . "/" . $file_path['basename']);
+
         return [
-            'buddhist_id'=>$this->buddhist_id,
-            'image'=>$anImage,
-            'buddhist_name'=>$this->buddhist->name,
-            'price'=>$this->buddhist->price,
-            'time'=>$this->created_at,
-            'read'=>$this->read
+            'buddhist_id' => $this->buddhist_id,
+            'image' => $anImage,
+            'buddhist_name' => $this->buddhist->name,
+            'data' => $this->buddhist->price,
+            'time' => $this->created_at,
+            'read' => $this->read,
+            'notification_type' => $this->notification_type,
+            'comment_path' => $this->comment_path,
 
         ];
     }
