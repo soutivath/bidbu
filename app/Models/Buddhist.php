d341b693e372942ffba149b8b10a16746d84564b
soutivath <www.soutivath2012@gmail.com>
2021-07-28 16:20:11 +0700

    fix end notificaion

--- app/Models/Buddhist.php
+++ app/Models/Buddhist.php
@@ -20,7 +20,7 @@ class Buddhist extends Model
     }
     public function user()
     {
-        return $this->belongsTo(User::class, "id", "user_id");
+        return $this->belongsTo(User::class, "user_id", "id");
 
     }
 
