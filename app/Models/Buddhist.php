52c6e6fa1cb14054ad851e889923a379db6a4a92
soutivath <www.soutivath2012@gmail.com>
2021-07-28 16:10:19 +0700

    fix end notificaion

--- app/Models/Buddhist.php
+++ app/Models/Buddhist.php
@@ -20,7 +20,7 @@ class Buddhist extends Model
     }
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, "id", "user_id");
 
     }
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable
     }
     public function buddhists()
     {
-        return $this->hasMany("app\Buddhist");
+        return $this->hasMany("app\Buddhist", "user_id");
     }
 
     public function notifications()
