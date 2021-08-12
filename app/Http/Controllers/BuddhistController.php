b6038c30b91595ee4804ce7627375a632ac313f5
soutivath <www.soutivath2012@gmail.com>
2021-08-13 03:50:29 +0700

    fix send

--- app/Http/Controllers/BuddhistController.php
+++ app/Http/Controllers/BuddhistController.php
@@ -539,7 +539,7 @@ class BuddhistController extends Controller
     public function checkBuddhistResult($id)
     {
 
-        $buddhist = Buddhist::where("id", $id)->with("user")->get();
+        $buddhist = Buddhist::where("id", $id)->with("user")->first();
 
         // return response()->json(["data" => $buddhist], 200);
         return new checkBuddhistResultResource($buddhist);
