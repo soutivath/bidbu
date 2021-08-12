e693431a775aabc9634d1b410feed3e7a25dca0b
soutivath <www.soutivath2012@gmail.com>
2021-08-12 16:30:26 +0700

    fix surname in result

--- app/Http/Resources/checkBuddhistResultResource.php
+++ app/Http/Resources/checkBuddhistResultResource.php
@@ -29,6 +29,7 @@ class checkBuddhistResultResource extends JsonResource
         return [
             "text" => $this->winner_user_id == Auth::user()->firebase_uid ? "ທ່ານຊະນະການປະມູນໃນຄັ້ງນີ້" : "ທ່ານແພ້ການປະມູນໃນຄັ້ງນີ້",
             "winner_name" => empty($winner_name) ? "ບໍ່ມີຜູ້ຊະນະ" : $winner_name->name,
+            "winner_surname" => empty($winner_name) ? "ບໍ່ມີຜູ້ຊະນະ" : $winner_name->surname,
             "winner_price" => $this->highest_price,
             "owner_name" => $this->user->name,
             "owner_phone" => $this->user->phone_number,
