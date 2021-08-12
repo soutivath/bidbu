1e9716a215f0c032fe9def3fde8c7018ea6e6a7b
soutivath <www.soutivath2012@gmail.com>
2021-08-13 03:51:48 +0700

    fix send

--- app/Http/Resources/checkBuddhistResultResource.php
+++ app/Http/Resources/checkBuddhistResultResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use App\Models\User;
 use Auth;
+use File;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class checkBuddhistResultResource extends JsonResource
@@ -16,12 +17,12 @@ class checkBuddhistResultResource extends JsonResource
      */
     public function toArray($request)
     {
-        /* $allImage = array();
+        $allImage = array();
         $files = File::files(public_path('/buddhist_images/' . $this->image_path . "/"));
         $file_path = pathinfo($files[0]);
         \array_push($allImage, Config("values.APP_URL") . ":" . $_SERVER["SERVER_PORT"] .
-        "/" . "buddhist_images/" . $this->image_path . "/" . $file_path['basename']);
-        $winner_name = User::where("firebase_uid", $this->winner_user_id)->first();*/
+            "/" . "buddhist_images/" . $this->image_path . "/" . $file_path['basename']);
+        $winner_name = User::where("firebase_uid", $this->winner_user_id)->first();
         return [
             "text" => $this->winner_user_id == Auth::user()->firebase_uid ? "ທ່ານຊະນະການປະມູນໃນຄັ້ງນີ້" : "ທ່ານແພ້ການປະມູນໃນຄັ້ງນີ້",
             "winner_name" => empty($winner_name) ? "ບໍ່ມີຜູ້ຊະນະ" : $winner_name->name,
