e2c84a3d4018bc5a91bfef753fd12f384da96a8f
soutivath <www.soutivath2012@gmail.com>
2021-08-15 18:56:21 +0700

    get auth password

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -37,7 +37,7 @@ class ProfileController extends Controller
             'picture' => 'sometimes|image|mimes:jpeg,png,jpg|max:8192',
             'password' => 'required',
         ]);
-        dd(Auth::user());
+        return response()->json(["data" => Auth::user()], 200);
         $user = User::find(Auth::id());
 
         if (!Hash::check($request->password, Auth::user()->password)) {
