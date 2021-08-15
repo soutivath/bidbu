e330a7ff5e41ecda3f8350f2dc88aaf67248fca7
soutivath <www.soutivath2012@gmail.com>
2021-08-15 18:07:43 +0700

    add edit profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'surname', 'email', 'password', 'phone_number', 'picture', 'firebase_uid',
+        'id', 'name', 'surname', 'email', 'password', 'phone_number', 'picture', 'firebase_uid',
     ];
 
     protected $primaryKey = 'id';
