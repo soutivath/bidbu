ead2dc1bef53fba42798f18f9c075f7a19f9975a
soutivath chanthaveexay <www.soutivath2012@gmail.com>
2022-03-31 17:35:17 +0700

    fix providor

--- app/Providers/RepositoryServiceProvidor.php
+++ app/Providers/RepositoryServiceProvidor.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 class RepositoryServiceProvidor extends ServiceProvider
 {
     public function register(){
