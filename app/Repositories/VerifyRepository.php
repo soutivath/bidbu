a5fc6208a6f75e9a2b3974737bb3f6196549e3fc
soutivath chanthaveexay <www.soutivath2012@gmail.com>
2022-05-03 23:40:50 +0700

    add debug

--- app/Repositories/VerifyRepository.php
+++ app/Repositories/VerifyRepository.php
@@ -353,18 +353,34 @@ class VerifyRepository implements VerifyInterface
             $oldIdTokenString = $request->old_firebase_token;
             try { // Try to verify the Firebase credential token with Google
                 $verifiedIdToken = $auth->verifyIdToken($idTokenString);
-                $verifiedOldIdToken = $auth->verifyIdToken($oldIdTokenString);
+               
             } catch (\InvalidArgumentException $e) { // If the token has the wrong format
                 return response()->json(
                     [
-                        'message' => 'Unauthorized - Can\'t parse the token: ' . $e->getMessage(),
+                        'message' => 'new token Unauthorized - Can\'t parse the token: ' . $e->getMessage(),
                     ],
                     401
                 );
             } catch (InvalidToken $e) { // If the token is invalid (expired ...)
 
                 return response()->json([
-                    'message' => 'Unauthorized - Token is invalid: ' . $e->getMessage(),
+                    'message' => 'new token Unauthorized - Token is invalid: ' . $e->getMessage(),
+                ], 401);
+            }
+            try { // Try to verify the Firebase credential token with Google
+               
+                $verifiedOldIdToken = $auth->verifyIdToken($oldIdTokenString);
+            } catch (\InvalidArgumentException $e) { // If the token has the wrong format
+                return response()->json(
+                    [
+                        'message' => ' old token Unauthorized - Can\'t parse the token: ' . $e->getMessage(),
+                    ],
+                    401
+                );
+            } catch (InvalidToken $e) { // If the token is invalid (expired ...)
+
+                return response()->json([
+                    'message' => ' old token Unauthorized - Token is invalid: ' . $e->getMessage(),
                 ], 401);
             }
 
